749a49359c648f7159ebb85eac608a3c7f3b7a1b
Jose Landim <joselandimsobrinho@hotmail.com>
2025-11-21 16:13:19 -0300

    Resolução do desafio 1

--- src/main/java/org/example/MinhaAnalise.java
+++ src/main/java/org/example/MinhaAnalise.java
@@ -1,22 +1,70 @@
 package org.example;
 
-import br.edu.icev.aed.forense.AnaliseForenseAvancada;
 import br.edu.icev.aed.forense.Alerta;
-import java.util.*;
+import br.edu.icev.aed.forense.AnaliseForenseAvancada;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class MinhaAnalise implements AnaliseForenseAvancada{
 
     // ----------------------------------------------------------------------
-    // Desafio 1: Encontrar Sessões Inválidas (Exemplo de Estrutura)
+    // Desafio 1: Encontrar Sessões Inválidas
     // ----------------------------------------------------------------------
 
     @Override
     public Set<String> encontrarSessoesInvalidas(String caminhoArquivo) throws IOException {
         Set<String> sessoesInvalidas = new HashSet<>();
 
-        //resolução desafio
+        Map<String, Deque<String>> pilhas = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+            String linha = br.readLine();
+            //processar cada linha
+            while ((linha = br.readLine()) != null) {
+                String[] campos = linha.split(",", -1);
+                String userId = campos[1];
+                String sessionId = campos[2];
+                String action = campos[3];
 
+                Deque<String> pilha = pilhas.get(userId);
+                //adicionar pilha do usuario e fazer as verificações
+                if (pilha == null) {
+                    pilha = new ArrayDeque<>();
+                    pilhas.put(userId, pilha);
+                }
+                //logica do login
+                if (action.equals("LOGIN")) {
+
+                    if (!pilha.isEmpty()) {
+                        sessoesInvalidas.add(sessionId);
+                    }
+                    pilha.push(sessionId);
+                }
+                //logica do logout
+                else if (action.equals("LOGOUT")) {
+                    //se a pilha ta vazia
+                    if (pilha.isEmpty()) {
+                        sessoesInvalidas.add(sessionId);
+                    }
+                    else {
+                        if (!pilha.peek().equals(sessionId)) {
+                            sessoesInvalidas.add(sessionId);
+                        }
+                        else {
+                            pilha.pop();
+                        }
+                    }
+                }
+            }
+        }
+        //sessoes que ficaram abertas
+        for (Deque<String> pilha : pilhas.values()) {
+            while (!pilha.isEmpty()) {
+                sessoesInvalidas.add(pilha.pop());
+            }
+        }
         return sessoesInvalidas;
     }
 
