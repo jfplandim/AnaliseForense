74deacd4b3fcdb929e7b6b62a3bde3a89ea9061f
Jose Landim <joselandimsobrinho@hotmail.com>
2025-11-21 18:03:36 -0300

    Correção no desafio 1

--- src/main/java/org/example/MinhaAnalise.java
+++ src/main/java/org/example/MinhaAnalise.java
@@ -3,8 +3,6 @@ package org.example;
 import br.edu.icev.aed.forense.Alerta;
 import br.edu.icev.aed.forense.AnaliseForenseAvancada;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,60 +10,58 @@ import java.util.*;
 
 public class MinhaAnalise implements AnaliseForenseAvancada{
 
+    private final LeituraCSV leitura = new LeituraCSV();
+
     // ----------------------------------------------------------------------
     // Desafio 1: Encontrar Sessões Inválidas
     // ----------------------------------------------------------------------
 
     @Override
     public Set<String> encontrarSessoesInvalidas(String caminhoArquivo) throws IOException {
+        List<Alerta> logs = leitura.getAlertas(caminhoArquivo);
+
+
         Set<String> sessoesInvalidas = new HashSet<>();
 
         Map<String, Deque<String>> pilhas = new HashMap<>(2048);
-        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo), 32768)) {
-            String linha = br.readLine();
-            //processar cada linha
-            while ((linha = br.readLine()) != null) {
-                //uso do indexOf em vez do split para otimização da leitura
-                int p1 = linha.indexOf(',');
-                int p2 = linha.indexOf(',', p1 + 1);
-                int p3 = linha.indexOf(',', p2 + 1);
-                int p4 = linha.indexOf(',', p3 + 1);
+        for (Alerta a: logs){
 
-                String userId = linha.substring(p1 + 1, p2);
-                String sessionId = linha.substring(p2 + 1, p3);
-                String action = linha.substring(p3 + 1, p4);
+            String user = a.getUserId();
+            String session = a.getSessionId();
+            String action = a.getActionType();
 
-                Deque<String> pilha = pilhas.get(userId);
+
+            Deque<String> pilha = pilhas.get(user);
                 //adicionar pilha do usuario e fazer as verificações
                 if (pilha == null) {
                     pilha = new ArrayDeque<>();
-                    pilhas.put(userId, pilha);
+                    pilhas.put(user, pilha);
                 }
                 //logica do login
                 if (action.equals("LOGIN")) {
 
                     if (!pilha.isEmpty()) {
-                        sessoesInvalidas.add(sessionId);
+                        sessoesInvalidas.add(session);
                     }
-                    pilha.push(sessionId);
+                    pilha.push(session);
                 }
                 //logica do logout
                 else if (action.equals("LOGOUT")) {
                     //se a pilha ta vazia
                     if (pilha.isEmpty()) {
-                        sessoesInvalidas.add(sessionId);
+                        sessoesInvalidas.add(session);
                     }
                     else {
-                        if (!pilha.peek().equals(sessionId)) {
-                            sessoesInvalidas.add(sessionId);
+                        if (!pilha.peek().equals(session)) {
+                            sessoesInvalidas.add(session);
                         }
                         else {
                             pilha.pop();
                         }
                     }
                 }
-            }
         }
+
         //sessoes que ficaram abertas
         for (Deque<String> pilha : pilhas.values()) {
             while (!pilha.isEmpty()) {
