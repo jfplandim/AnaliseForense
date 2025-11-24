0ff3683c4a745957c75f61de9e1cbd93bcb7213d
João <joaoguimalta2712@gmail.com>
2025-11-23 21:49:22 -0300

    Melhorando a class de LeituraCSV.java

--- src/main/java/org/example/LeituraCSV.java
+++ src/main/java/org/example/LeituraCSV.java
@@ -18,11 +18,9 @@ public class LeituraCSV {
         alertas = new ArrayList<>(500_000);
 
         try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo), 32768)) {
-
             String linha = br.readLine(); // pula header
 
             while ((linha = br.readLine()) != null) {
-
                 int p1 = linha.indexOf(',');
                 int p2 = linha.indexOf(',', p1 + 1);
                 int p3 = linha.indexOf(',', p2 + 1);
@@ -30,17 +28,19 @@ public class LeituraCSV {
                 int p5 = linha.indexOf(',', p4 + 1);
                 int p6 = linha.indexOf(',', p5 + 1);
 
-                long timestamp = Long.parseLong(linha.substring(0, p1));
+                // Ordem correta conforme header CSV:
+                // ALERT_ID, USER_ID, IP_ADDRESS, SESSION_ID, TARGET_RESOURCE, SEVERITY, TIMESTAMP
+                long alertId = Long.parseLong(linha.substring(0, p1));
                 String userId = linha.substring(p1 + 1, p2);
-                String sessionId = linha.substring(p2 + 1, p3);
-                String action = linha.substring(p3 + 1, p4);
-                String target = linha.substring(p4 + 1, p5);
+                String ipAddress = linha.substring(p2 + 1, p3);
+                String sessionId = linha.substring(p3 + 1, p4);
+                String targetResource = linha.substring(p4 + 1, p5);
                 int severity = Integer.parseInt(linha.substring(p5 + 1, p6));
-                long bytes = Long.parseLong(linha.substring(p6 + 1));
+                long timestamp = Long.parseLong(linha.substring(p6 + 1));
 
                 alertas.add(new Alerta(
-                        timestamp, userId, sessionId,
-                        action, target, severity, bytes
+                        alertId, userId, ipAddress, sessionId,
+                        targetResource, severity, timestamp
                 ));
             }
         }
@@ -48,5 +48,4 @@ public class LeituraCSV {
         carregado = true;
         return alertas;
     }
-
 }
